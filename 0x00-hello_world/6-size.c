9649782b346b78a12704d4cbf73e048262a4a51a
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2023-02-16 09:51:04 -0800

    Print Size of Various Types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,12 +1,11 @@
-#insert <stdio.h>
+#include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Success)
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
