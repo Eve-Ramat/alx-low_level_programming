b5b401ae0e67d30ff3596ab15a86b999ce5fd86b
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2023-02-16 08:59:06 -0800

    Use printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,9 @@
  * main - Entry point
  * Return: Always 0 (success)
  */
-
+i
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece if art,\n");
 		return(0);
 }
