5bdbd885f489c8a13aca6025fcbb16288a8c3092
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2023-02-16 08:30:34 -0800

    Write C Program Using the puts Function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this prints Programming is like building a multilingual puzzle, followed by a new line.
+ * Return: this returns 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0)
+}
