8daab2e252303fdabd4ecdeb7b45081ab5447eaf
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2023-12-15 12:57:52 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,17 +13,19 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	last_n = n % 10;
+
 	if (n > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_n);
 	}
 	else if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n);
+		printf("Last digit of %d is %d and is 0\n", n, last_n);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_n);
 	}
 	return (0);
 }
