bfb40896169638a331b8723bd5b47bac67bcdf23
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2023-12-16 07:52:54 -0800

    1-last_digit.c4

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	}
 	else if (last_n == 0)
 	{
-		printf("Last digit of %d is %d and is  0\n", n, last_n);
+		printf("Last digit of %d is %d and is 0\n", n, last_n);
 	}
 	else if (last_n < 6 && last_n != 0)
 	{
