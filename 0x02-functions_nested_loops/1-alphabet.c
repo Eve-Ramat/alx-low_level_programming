31a9d59ff76d0b210ce2bcaa52ff0419cf793baf
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2023-12-19 14:09:42 -0800

    1-alphabet.c1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints alphabets in lowercase
+* Return: void
+*/
+
+void print_alphabet(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		_putchar(alphabets);
+	}
+	_putchar('\n');
+}
