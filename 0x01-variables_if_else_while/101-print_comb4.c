cb67c248e7a41997bbe55c782b03045bd52189da
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2023-12-29 09:41:04 -0800

    101-print_comb4.c4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,44 +1,45 @@
 #include <stdio.h>
 
 /**
- * main - Entry point, prints possible outcomes of three digits
- * Return: 0 on successful execution
+ * main - prints possible outcomes of three digits
+ * Return: 0 if successful
  */
 int main(void)
 {
-	int i; /* Declare i here */
-	int j; /* Declare j here */
-	int k; /* Declare k here */
+	int i, j, k;  /* Declare variables for digits */
 
-	i = 48; /* Start i at 48 */
-	j = i + 1; /* Start j at i + 1 */
-	k = j + 1; /* Start k at j + 1 */
+	i = 48;  /* Initialize i to ASCII value of '0' */
+	j = 48;  /* Initialize j to ASCII value of '0' */
+	k = 48;  /* Initialize k to ASCII value of '0' */
 
-	while (i <= 55) /* Loop for i from 48 to 55 */
+	while (i <= 55)  /* Loop for values from '0' to '7' */
 	{
-		while (j <= 56) /* Loop for j from i + 1 to 56 */
+		j = i + 1;  /* Start j from the next value of i */
+		while (j <= 56)  /* Loop for values from j to '8' */
 		{
-			while (k <= 57) /* Loop for k from j + 1 to 57 */
+			k = j + 1;  /* Start k from the next value of j */
+			while (k <= 57)  /* Loop for values from k to '9' */
 			{
-				putchar(i);
-				putchar(j);
-				putchar(k);
+				putchar(i);  /* Print the character for i */
+				putchar(j);  /* Print the character for j */
+				putchar(k);  /* Print the character for k */
 
-				if (i <= 54 || j <= 55 || k <= 56) /* Check if not the last combination */
+				/* Check if it's not the last combination to print comma and space */
+				if (i <= 54 || j <= 55 || k <= 56)
 				{
-					putchar(44); /* Print comma */
-					putchar(32); /* Print space */
+					putchar(44);  /* Print comma */
+					putchar(32);  /* Print space */
 				}
 
-				k++; /* Move to the next k value */
+				k++;  /* Increment k for the next iteration */
 			}
-			j++; /* Move to the next j value */
+			j++;  /* Increment j for the next iteration */
 		}
-		i++; /* Move to the next i value */
+		i++;  /* Increment i for the next iteration */
 	}
 
-	putchar(10); /* Print a newline */
+	putchar(10);  /* Print a newline character */
 
-	return (0); /* Return 0 to indicate successful execution */
+	return (0);  /* Return 0 to indicate successful execution */
 }
 
