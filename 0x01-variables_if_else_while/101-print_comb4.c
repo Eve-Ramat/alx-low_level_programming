d5418478c3662fd0acb24552b18fdc3e0e005728
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2023-12-29 09:25:50 -0800

    101-print_comb4.c3

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,43 +1,44 @@
 #include <stdio.h>
 
 /**
- * main - prints possible outcomes of three digits
- * Return: 0 if (successful)
+ * main - Entry point, prints possible outcomes of three digits
+ * Return: 0 on successful execution
  */
-
 int main(void)
 {
-	int i, j, k;
+	int i; /* Declare i here */
+	int j; /* Declare j here */
+	int k; /* Declare k here */
 
-	i = 48;
-	j = 48;
-	k = 48;
+	i = 48; /* Start i at 48 */
+	j = i + 1; /* Start j at i + 1 */
+	k = j + 1; /* Start k at j + 1 */
 
-	while (i <= 55)
+	while (i <= 55) /* Loop for i from 48 to 55 */
 	{
-		j = i + 1;
-		while (j <= 56)
+		while (j <= 56) /* Loop for j from i + 1 to 56 */
 		{
-			k = j + 1;
-			while (k <= 57)
+			while (k <= 57) /* Loop for k from j + 1 to 57 */
 			{
 				putchar(i);
 				putchar(j);
 				putchar(k);
-				if (i <= 54 || j <= 55 || k <= 56)
+
+				if (i <= 54 || j <= 55 || k <= 56) /* Check if not the last combination */
 				{
-					putchar(44);
-					putchar(32);
+					putchar(44); /* Print comma */
+					putchar(32); /* Print space */
 				}
-				k++;
+
+				k++; /* Move to the next k value */
 			}
-			j++;
+			j++; /* Move to the next j value */
 		}
-		i++;
+		i++; /* Move to the next i value */
 	}
 
-	putchar(10);
+	putchar(10); /* Print a newline */
 
-	return (0);
+	return (0); /* Return 0 to indicate successful execution */
 }
 
