0d754f7ede425dc934f65ec8f299e8925c32f898
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2023-12-29 10:06:01 -0800

    101-print_comb4.c5

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,20 +4,21 @@
  * main - prints possible outcomes of three digits
  * Return: 0 if successful
  */
+
 int main(void)
 {
-	int i, j, k;  /* Declare variables for digits */
+	int i;  /* Declare i outside the loop */
 
 	i = 48;  /* Initialize i to ASCII value of '0' */
-	j = 48;  /* Initialize j to ASCII value of '0' */
-	k = 48;  /* Initialize k to ASCII value of '0' */
 
 	while (i <= 55)  /* Loop for values from '0' to '7' */
 	{
-		j = i + 1;  /* Start j from the next value of i */
+		int j = i + 1;  /* Declare and initialize j to the next value of i */
+
 		while (j <= 56)  /* Loop for values from j to '8' */
 		{
-			k = j + 1;  /* Start k from the next value of j */
+			int k = j + 1;  /* Declare and initialize k to the next value of j */
+
 			while (k <= 57)  /* Loop for values from k to '9' */
 			{
 				putchar(i);  /* Print the character for i */
@@ -31,11 +32,11 @@ int main(void)
 					putchar(32);  /* Print space */
 				}
 
-				k++;  /* Increment k for the next iteration */
+				k++;  /* Move to the next k value */
 			}
-			j++;  /* Increment j for the next iteration */
+			j++;  /* Move to the next j value */
 		}
-		i++;  /* Increment i for the next iteration */
+		i++;  /* Move to the next i value */
 	}
 
 	putchar(10);  /* Print a newline character */
