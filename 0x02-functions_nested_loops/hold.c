0608acc785a13d49b40a8f5f518661bb39b4eec3
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-01-01 15:17:51 -0800

    4-isalpha.c4

--- 0x02-functions_nested_loops/hold.c
+++ 0x02-functions_nested_loops/hold.c
@@ -8,5 +8,12 @@
 
 int _isalpha(int c)
 {
-return (((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) ? 1 : 0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
