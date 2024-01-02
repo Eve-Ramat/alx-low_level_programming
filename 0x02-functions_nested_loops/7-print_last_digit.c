21421269e138bc115cdf82632303cb845f38e624
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-01-01 17:49:10 -0800

    7-print_last_digit.c3

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,7 +12,9 @@ int print_last_digit(int number)
 	if (number < 0)
 		number = -number;
 
-	int last_digit = number % 10;
+	int last_digit;
+
+	last_digit = number % 10;
 
 	_putchar(last_digit + '0');
 	return (last_digit);
