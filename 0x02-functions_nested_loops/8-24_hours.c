a6b5fa491fc1c8b6a29fbc4262375e06cea932bc
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-01-03 06:15:48 -0800

    8-24_hours.c3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,11 +13,11 @@ void jack_bauer(void)
 	{
 		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar(hour / 10);
-			_putchar(hour % 10);
+			_putchar((hour / 10) + 48);
+			_putchar((hour % 10) + 48);
 			_putchar(':');
-			_putchar(minute / 10);
-			_putchar(minute % 10);
+			_putchar((minute / 10) + 48);
+			_putchar((minute % 10) + 48);
 		}
 
 		_putchar(10);
