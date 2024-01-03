7f357e1b7eef1770b0b022c9b98d5251a520adbd
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-01-03 06:36:22 -0800

    8-24_hours.c4

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,25 +1,24 @@
 #include "main.h"
 
 /**
- * jack_bauer- every minute of the day of Jack Bauer.
- * Starting from 00:00 to 23:59.
+ * jack_bauer - Prints every minute of the day of Jack Bauer.
+ * The function prints the time from 00:00 to 23:59.
  */
-
 void jack_bauer(void)
 {
-	int hour, minute;
+    int hour, minute;
 
-	for (hour = 0; hour <= 23; hour++)
-	{
-		for (minute = 0; minute <= 59; minute++)
-		{
-			_putchar((hour / 10) + 48);
-			_putchar((hour % 10) + 48);
-			_putchar(':');
-			_putchar((minute / 10) + 48);
-			_putchar((minute % 10) + 48);
-		}
-
-		_putchar(10);
-	}
+    for (hour = 0; hour <= 23; hour++)
+    {
+        for (minute = 0; minute <= 59; minute++)
+        {
+            _putchar((hour / 10) + '0');
+            _putchar((hour % 10) + '0');
+            _putchar(':');
+            _putchar((minute / 10) + '0');
+            _putchar((minute % 10) + '0');
+        }
+        _putchar('\n');
+    }
 }
+
