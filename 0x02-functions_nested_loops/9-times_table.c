a9c5491e7c67023f015fa9e0eaf97890653c924c
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-01-05 06:58:35 -0800

    9-times_table.c6

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,9 +24,13 @@ void times_table(void)
 				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
 			}
-			else
+			else if (product < 10 && column != 0)
 			{
 				_putchar(' ');
+				_putchar((product % 10) + '0');
+			}
+			else
+			{
 				_putchar(product + '0');
 			}
 		}
