f7ef6181e4a3efa3009717ec5083c2e15e6c8e51
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-01-07 11:47:38 -0800

    100-times_table.c14

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -36,8 +36,10 @@ void print_times_table(int n)
 			else
 			{
 				if (product > 99)
+				{
 					_putchar(product / 100 + '0');
 					_putchar((product / 10 % 10) + '0');
+				}
 			}
 
 			_putchar(product % 10 + '0');
