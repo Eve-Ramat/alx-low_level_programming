1b2b9be0583817e7693748498a0a08123d8fc82a
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-01-17 13:04:10 -0800

    5-more_numbers.c3

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_multiplied_numbers - function that prints from 0 to 14, 10 times.
+ * more_numbers - function that prints from 0 to 14, 10 times.
  * followed by a new line.
  */
-void print_multiplied_numbers(void)
+void more_numbers(void)
 {
 	int iteration, current_number;
 
