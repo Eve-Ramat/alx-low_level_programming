cc69dcb649515e59605b870ad4a9e22b16faff07
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-01-28 14:52:56 -0800

    5-rev_string.c3

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,15 @@ void rev_string(char *s)
 {
 	int length = 0;
 	char temp;
+	int start;
+	int end;
 
 	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	for (int start = 0, end = length - 1; start < end; start++, end--)
+	for (start = 0, end = length - 1; start < end; start++, end--)
 	{
 		temp = s[start];
 		s[start] = s[end];
