53eac0dd9ec65788232fbe112dbcfb6ce771eae3
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-02-11 08:06:27 -0800

    replaced [i] with index

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,11 +9,11 @@
 
 void _puts(char *str)
 {
-int current_character;
+int index;
 
-for (current_character = 0; str[current_character] != '\0' ; current_character++)
+for (index = 0; str[index] != '\0' ; index++)
 {
-_putchar(str[current_character]);
+_putchar(str[index]);
 }
 _putchar('\n');
 }
