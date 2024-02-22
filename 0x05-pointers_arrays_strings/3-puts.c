9cd20395c1cdbaec12664ee46319d408d3c4ae59
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-02-22 10:28:58 -0800

    R5

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _puts - print a string
+ * _puts - prints a string followed by a new line
  * @str: the string to be printed
  *
  * Return: nothing
  */
-
 void _puts(char *str)
 {
-        int index = 0;
+	int index = 0;
 
-        while (str[index] != '\0')
-        {
-                _putchar(str[index]);
-                index++;
-        }
-        _putchar('\n');
+	while (str[index] != '\0')
+	{
+		_putchar(str[index]);
+		index++;
+	}
+	_putchar('\n');
 }
+
