6892110932111fd4de34b89da629682a2106f39a
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-02-22 10:25:07 -0800

    R4

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,12 +9,12 @@
 
 void _puts(char *str)
 {
-	int index = 0;
+        int index = 0;
 
-	while (str[index] != '\0')
-	{
-		index++;
-		_putchar(str[index]);
-	}
-	_putchar('\n');
+        while (str[index] != '\0')
+        {
+                _putchar(str[index]);
+                index++;
+        }
+        _putchar('\n');
 }
