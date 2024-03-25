45b4a15c114e12579396847d034791ecd9a8e344
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-25 12:19:08 -0700

    all three combinations possible

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* Return: 0
-*/
+ * main - Entry point
+ * Return: 0
+ */
 
 int main(void)
 {
 	int firstDigit, secondDigit, thirdDigit;
 
-	for (firstDigit = 0; firstDigit <= 7; firstDigit++)
+	for (firstDigit = '0'; firstDigit <= '7'; firstDigit++)
 	{
-		for (secondDigit = firstDigit + 1; secondDigit <= 8; secondDigit++)
+		for (secondDigit = firstDigit + 1; secondDigit <= '8'; secondDigit++)
 		{
-			for (thirdDigit = secondDigit + 1; thirdDigit <= 9; thirdDigit++)
+			for (thirdDigit = secondDigit + 1; thirdDigit <= '9'; thirdDigit++)
 			{
-				putchar(firstDigit + '0');
-				putchar(secondDigit + '0');
-				putchar(thirdDigit + '0');
+				putchar(firstDigit);
+				putchar(secondDigit);
+				putchar(thirdDigit);
 
-				if (firstDigit != 7 || secondDigit != 8 || thirdDigit != 9)
+				if (firstDigit < '7')
 				{
-					putchar(44);
-					putchar(32);
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
 	}
-
-	putchar(10);
-
+	putchar('\n');
 	return (0);
 }
-
-
