c5fc245d03ee03fdd84226f41bb6842a0de86370
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-26 22:14:09 -0700

    A+

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,7 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - prints alphabets in lowercase
- * Return: 0
  */
 
 void print_alphabet(void)
