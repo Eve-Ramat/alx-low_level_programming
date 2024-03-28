fe4fd376ddded110558b69c03f57c4807b2b9866
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-28 09:29:35 -0700

    999

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,9 @@
 #include "main.h"
 
 /**
- * times_table - Prints the 9 times table, starting with 0.
+ * times_table - prints the 9 times table, starting with 0.
  */
+
 void times_table(void)
 {
 	int row, column, product;
@@ -11,17 +12,26 @@ void times_table(void)
 	{
 		for (column = 0; column <= 9; column++)
 		{
-			column != 0 ? (_putchar(','), _putchar(32)) : 0;
-
 			product = row * column;
 
-			product >= 10 ?
-				(_putchar((product / 10) + '0'), _putchar((product % 10) + '0')) :
-				(product < 10 && column != 0) ?
-					(_putchar(32), _putchar(product + '0')) :
-					_putchar(product + '0');
+			if (product >= 0)
+			{
+				_putchar(44);
+				_putchar(32);
+			}
+
+			if (product <= 9)
+			{
+				_putchar(32);
+				_putchar(product + 48);
+			}
+			else if (product >= 10)
+			{
+				_putchar((product / 10) + 48);
+				_putchar((product % 10) + 48);
+			}
 		}
+
 		_putchar(10);
 	}
 }
-
