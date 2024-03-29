6ff26539faa4d620c01514973e62320d4963ff4e
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-29 04:39:20 -0700

    case 111

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,37 +4,41 @@
  * print_to_98 - prints all natural numbers from n to 98.
  * @n: starting number.
  */
-
 void print_to_98(int n)
 {
 	while (n < 0)
 	{
 		_putchar('-');
-		n > -10 ? (_putchar(-n + '0')) : (_putchar(n / 10 * -1 + '0'), _putchar(n % 10 * -1 + '0'));
-		n < 98 ? (_putchar(44), _putchar(32)) : 0;
+		n > -10 ? (_putchar(-n + '0')) :
+			(_putchar(n / 10 * -1 + '0'), _putchar(n % 10 * -1 + '0'));
+		n < 98 ? (_putchar(','), _putchar(' ')) : 0;
 		n++;
 	}
 
 	while (n <= 98 && n <= 9)
 	{
-		_putchar(n + 48);
-		n < 98 ? (_putchar(44), _putchar(32)) : 0;
+		_putchar(n + '0');
+		n < 98 ? (_putchar(','), _putchar(' ')) : 0;
 		n++;
 	}
-	while  (n <= 98 && n >= 10)
+
+	while (n <= 98 && n >= 10)
 	{
-		_putchar((n / 10) + 48);
-		_putchar((n % 10) + 48);
-		n < 98 ? (_putchar(44), _putchar(32)) : 0;
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		n < 98 ? (_putchar(','), _putchar(' ')) : 0;
 		n++;
 	}
-	while (n > 99)
+
+	while (n >= 98)
 	{
-		_putchar((n / 100) + 48);
-		_putchar((n / 10 % 10) + 48);
-		_putchar((n % 10) + 48);
-		n != 98 ? (_putchar(44), _putchar(32)) : 0;
+		n > 99 ? (_putchar((n / 100) + '0'), _putchar((n / 10 % 10) + '0'),
+			_putchar((n % 10) + '0')) :
+			(_putchar((n / 10) + '0'), _putchar((n % 10) + '0'));
+		n > 98 ? (_putchar(','), _putchar(' ')) : 0;
 		n--;
 	}
-	_putchar(10);
+
+	_putchar('\n');
 }
+
