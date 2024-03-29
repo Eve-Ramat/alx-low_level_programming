57a73786da8b80dce75d258d3e6245c2996a8421
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-29 15:34:56 -0700

    Time saving

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,17 +8,19 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
 	}
 	else
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d, ", n);
 		}
 	}
+
+	printf("98\n");
 }
 
