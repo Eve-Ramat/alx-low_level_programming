f833802b13743bbd1b6b099bcb6efac9c727218e
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-29 07:41:59 -0700

    Wisdom is profitable to direct

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,56 +6,28 @@
  */
 void print_to_98(int n)
 {
-	while (n < 0)
+	while (n != 98)
 	{
-		_putchar('-');
-		n > -10 ? (_putchar(-n + '0')) :
-		(_putchar(n / 10 * -1 + '0'),
-		_putchar(n % 10 * -1 + '0'));
-		n != 98 ? (_putchar(','), _putchar(' ')) : 0;
-		n++;
-	}
-
-	while (n <= 98 && n <= 9)
-	{
-		_putchar(n + '0');
-		n != 98 ? (_putchar(','), _putchar(' ')) : 0;
-		n++;
-	}
-
-	while (n <= 98 && n >= 10)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-		n != 98 ? (_putchar(','), _putchar(' ')) : 0;
-		n++;
-	}
-
-	while (n > 99)
-	{
-		_putchar((n / 100) + '0');
-		_putchar((n / 10 % 10) + '0');
-		_putchar((n % 10) + '0');
-		n != 98 ? (_putchar(','), _putchar(' ')) : 0;
-		n--;
-	}
-
-	if (n == 98)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-
-	while (n >= -111)
-	{
-		_putchar(n <= -100 ? '-' : (n / 100) * -1 + '0');
-		_putchar(n <= -100 ? ((n / 10 % 10) * -1) + '0' :
+		_putchar((n > 9 || n < -9) ?
+		(n / 100) + '0' : (n < 0 ? '-' :
+		(n / 10) * -1 + '0'));
+		_putchar((n > 9 || n < -9) ?
+		(n / 10 % 10) + '0' :
 		((n / 10) % 10) * -1 + '0');
-		_putchar((n % 10) * -1 + '0');
-		n != 98 ? (_putchar(','), _putchar(' ')) : 0;
-		n++;
-	}
+		_putchar((n % 10) * (n < 0 ? -1 : 1) + '0');
 
+		if (n != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		if (n < 98)
+			n++;
+		else
+			n--;
+	}
+	_putchar('9');
+	_putchar('8');
 	_putchar('\n');
 }
 
