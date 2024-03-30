3695076ea10193be60afe21c9fc7ad7560cb9a29
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-30 07:37:31 -0700

    Ternary Operator

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,6 +16,7 @@ for (row = 0; row <= n; row++)
 for (column = 0; column <= n; column++)
 {
 product = row * column;
+column == 0 ? _putchar(product + '0') : 0;
 
 if (column != 0)
 {
@@ -29,13 +30,14 @@ _putchar(' ');
 _putchar(' ');
 _putchar(product + '0');
 }
-else if (product >= 10 && product <= 99)
+else if (product >= 10 && product < 100)
 {
 _putchar(' ');
 _putchar((product / 10) + '0');
 _putchar((product % 10) + '0');
 }
-else
+
+if (product >= 100)
 {
 _putchar((product / 100) + '0');
 _putchar((product / 10 % 10) + '0');
