1c85b8896c023f1f4c4c500dc0bb88f6cd48d440
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-03-30 05:45:28 -0700

    check100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,49 +1,48 @@
 #include "main.h"
 
 /**
- * print_times_table - Prints the multiplication table up to n.
- * @n: The maximum value up to which the table will be printed.
+ * print_times_table - prints the n times table.
+ * @n: number of timetable block to be printed.
  */
 void print_times_table(int n)
 {
-	int row, column;
+int row, column, product;
 
-	if (n < 0 || n > 15)
-		return;
+if (n >= 0 && n <= 15)
+{
+for (row = 0; row <= n; row++)
+{
+for (column = 0; column <= n; column++)
+{
+product = row * column;
 
-	for (row = 0; row <= n; row++)
-	{
-		for (column = 0; column <= n; column++)
-		{
-			int product = row * column;
-
-			if (column != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-
-				if (product <= 9)
-					_putchar(' ');
-			}
-
-			if (product > 99)
-			{
-				_putchar(product / 100 + '0');
-				_putchar((product / 10 % 10) + '0');
-			}
-			else if (product > 9)
-			{
-				_putchar(' ');
-				_putchar(product / 10 + '0');
-			}
-			else
-			{
-				if (column != 0)
-					_putchar(' ');
-			}
-
-			_putchar(product % 10 + '0');
-		}
-		_putchar('\n');
-	}
+if (column != 0)
+{
+_putchar(',');
+_putchar(' ');
 }
+
+if (product < 10 && column != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(product + '0');
+}
+else if (product >= 10 && product <= 99)
+{
+_putchar(' ');
+_putchar((product / 10) + '0');
+_putchar((product % 10) + '0');
+}
+else
+{
+_putchar((product / 100) + '0');
+_putchar((product / 10 % 10) + '0');
+_putchar((product % 10) + '0');
+}
+}
+_putchar('\n');
+}
+}
+}
+
