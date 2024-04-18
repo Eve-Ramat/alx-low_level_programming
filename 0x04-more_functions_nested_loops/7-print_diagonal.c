2f8a204f9ebcb177f02fca23fd80208f627b1afd
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-04-18 08:06:11 -0700

    771

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- * print_diagonal - prints a diagonal line on the terminal
- * @n: number of times the character '\' should be printed
- *
- * Return: nothing
+ * print_diagonal - Prints a diagonal line on the terminal.
+ * @n: The number of times the character '\' should be printed.
  */
 
 void print_diagonal(int n)
@@ -17,9 +15,9 @@ void print_diagonal(int n)
 	{
 		int slashes, spaces;
 
-		for (slashes = 0; slashes < n; slashes++)
+		for (slashes = 1; slashes <= n; slashes++)
 		{
-			for (spaces = 0; spaces < slashes; spaces++)
+			for (spaces = 1; spaces < slashes; spaces++)
 			{
 				_putchar(32);
 			}
