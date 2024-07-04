04214e2582db2d384f9e32103886347ecf4a7ead
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-07-03 18:57:39 -0700

    9 Retry

--- 0x04-more_functions_nested_loops/cp9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/cp9-fizz_buzz.c
@@ -11,9 +11,6 @@ int main(void)
 
 	for (number = 1; number <= 100; number++)
 	{
-		if (number != 100)
-			putchar(32);
-
 		if (number % 3 == 0 && number % 5 == 0)
 			printf("FizzBuzz");
 		else if (number % 3 == 0)
@@ -22,6 +19,9 @@ int main(void)
 			printf("Buzz");
 		else
 			printf("%d", number);
+
+		if (number != 100)
+			putchar(32);
 	}
 
 	printf("\n");
