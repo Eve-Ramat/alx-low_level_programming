89f41611061d26a2970f5b9aebedcf0cf317ed23
Ramatu Aliyu Mustapha <ramatualiyu123@gmail.com>
2024-07-05 05:52:54 -0700

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,17 +7,16 @@
 
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-	}
+unsigned int num = n;
 
-	if (n / 10 != 0)
-	{
-		print_number(n / 10);
-	}
-
-	_putchar((n % 10) + '0');
+if (n < 0)
+{
+_putchar('-');
+num = -num;
 }
-
+if ((num / 10) > 0)
+{
+print_number(num / 10);
+}
+_putchar((num % 10) + '0');
+}
